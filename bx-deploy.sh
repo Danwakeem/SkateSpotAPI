63e27a58dab5545bed186efbeaed013c55ee3fee
Danwakeem <danwakeem@gmail.com>
2018-06-01 00:00:09 -0500

    Fixed API Function name

--- bx-deploy.sh
+++ bx-deploy.sh
@@ -1,4 +1,4 @@
 bx login -a api.ng.bluemix.net --apikey "${apikey}" -o "${user}" -s "${space}"
 
-bx wsk action update API/CreateSpots post/spot/index.js --kind nodejs:8
+bx wsk action update API/CreateSpot post/spot/index.js --kind nodejs:8
 bx wsk action update API/GetSpots get/spot/index.js --kind nodejs:8
